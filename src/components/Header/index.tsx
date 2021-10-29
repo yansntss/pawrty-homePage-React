378ca2fe88cadd106a34ec60cd0c660c1eee2614
yansntss <yansantos.dev@gmail.com>
2021-10-29 14:22:26 -0300

    initials

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./styles.module.scss";
+import logo from "../../assets/Logo.svg";
+import aranha from "../../assets/Aranha.png";
+import teiaHeader from "../../assets/TeiaHeader.svg";
+
+export function Header() {
+  return (
+    <div className={styles.header}>
+      <img className={styles.logo} src={logo} alt="logo" />
+      <img className={styles.aranha} src={aranha} alt="aranha" />
+
+      <div className={styles.list}>
+        <ul>
+          <li>Pawrty</li>
+          <li>Pricing</li>
+          <li>About us</li>
+          <li>Reviews</li>
+        </ul>
+      </div>
+
+      <img src={teiaHeader} alt="" className={styles.teiaHeader} />
+    </div>
+  );
+}
